5fc455dc678ccb4de2a4e1a4a7c8f50129a99d17
Jeroen Dalsem <jdalsem@coldtrick.com>
2023-11-17 15:58:48 +0100

    changed: slideshow will now default to autoplay

--- views/default/widgets/image_slideshow/content.php
+++ views/default/widgets/image_slideshow/content.php
@@ -57,7 +57,7 @@ $slides .= elgg_format_element('div', ['class' => 'slide-previous'], elgg_view_i
 $id = "slideshow_{$widget->guid}";
 
 $container_class = ['widget-slideshow-container'];
-if ((bool) $widget->autoplay) {
+if ($widget->autoplay !== '0') { // null == autplay
 	$container_class[] = 'slideshow-autoplay';
 }
 
